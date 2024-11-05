d8c5f4e6882b8f9d9db7acc4e869db9f7fbe968e
Akrati Kushwah <70066917+akratikushwah@users.noreply.github.com>
2024-11-05 08:38:51 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version of js
+
+// alert(3+3) //we are using nodejs , not browser
+
+let name = "Akrati" //string
+let age = 18 //number
+let IsLoggedIn = false //boolean
+
+/*
+number = 2 ^ 53 (range)
+bigint = for large numbers
+string = "assembly of characters"
+boolean = true or false
+null = stand alone value (type = object)
+undefined = kindda place holder for a value which is not defined yet (type = undefined)
+symbol = unique
+*/
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
