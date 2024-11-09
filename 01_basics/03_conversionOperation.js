b4cbe4fcac6d86eacc55d767703e8674855a92c9
Akrati Kushwah <70066917+akratikushwah@users.noreply.github.com>
2024-11-09 12:41:57 +0000

    Operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,4 +26,42 @@ let someNumber = 34
 
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+
+// ******************************** Operations ***************************
+
+let value = 3
+
+let negValue = -value
+
+//console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Akrati"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log('1' + 3);
+
+console.log("2" + "4");
+
+console.log("1" + 2 + 2)
+
+console.log(1 + 2 + "2")
+
+console.log(true + 3);
+
+console.log(+true)
+
+console.log(+"")
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter)
+
+
