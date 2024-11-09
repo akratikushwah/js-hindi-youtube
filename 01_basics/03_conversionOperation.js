21e31aa62537db86467ad5f294a8e2d93d4eb334
Akrati Kushwah <70066917+akratikushwah@users.noreply.github.com>
2024-11-09 12:21:34 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33abc" // Nan -> not a number
+
+//console.log(typeof score)
+
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// 33 => Number
+// "33abc" => Nan
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 =>false
+//" " => false
+// "Akrati" => true
+
+let someNumber = 34
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
