4f8457d1c6ddd798f25fca5dd2fba7eec779e475
Akrati Kushwah <70066917+akratikushwah@users.noreply.github.com>
2024-11-18 11:49:48 +0000

    push

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,7 +1,43 @@
 // Arrays
 
-const myArr = [0 ,1 , 2, 3, 4, 5]
+const myArr = [0, 1, 2, 3, 4, 5]
 
-const myHeroes = ["Shaktiman" , "Hanuman"]
+const myHeroes = ["Shaktiman", "Hanuman"]
 
-console.log(myArr[0]);
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[0]); //0
+console.log(myArr2[0]); //1
+
+//Array Methods
+
+myArr.push(6)
+console.log(myArr); // 0 ,1 , 2,3, 4, 5, 6
+myArr.push(7)
+console.log(myArr); // 0 ,1 , 2,3, 4, 5, 6 , 7
+
+myArr.pop()
+console.log(myArr); // 0 ,1 , 2, 3, 4, 5, 6
+
+myArr.unshift(9)
+
+console.log(myArr); //9 , 0 , 1 , 2, 3, 4, 5, 6 -> it will add 9 at the start of an array and all the already existing elements in the array will be shifted by 1
+
+myArr.shift()
+
+console.log(myArr); // 0 , 1 , 2, 3, 4, 5 , 6 -> it will remove the first element
+
+console.log(myArr.includes(9)); // false -> it will give output in boolean
+
+console.log(myArr.indexOf(9)); // -1 --> 9 does not exist in an array
+
+
+
+
+
+
+
+
+
+
+
+
