855c7d7ead4066fa833203f5223a6a9a340617c1
Akrati Kushwah <70066917+akratikushwah@users.noreply.github.com>
2024-11-18 15:22:55 +0000

    arr

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,9 +1,9 @@
 const marvel_heroes = ["Thor" , "Ironman" , "Spiderman"]
 
-const dc_heroes = ["Spiderman" , "Flash" , "Batman"]
+const dc_heroes = ["hanuman" , "Flash" , "Batman"]
 
 marvel_heroes.push(dc_heroes)
 
-console.log(marvel_heroes); // [ 'Thor', 'Ironman', 'Spiderman', [ 'Spiderman', 'Flash', 'Batman' ] ]
+console.log(marvel_heroes); // [ 'Thor', 'Ironman', 'Spiderman', [ 'hanuman', 'Flash', 'Batman' ] ]
 
 
