e9186c9e8b399d706064a02845f36fd4bdc1fec5
Akrati Kushwah <70066917+akratikushwah@users.noreply.github.com>
2024-11-25 16:21:23 +0000

    obj

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -2,13 +2,13 @@ const tinderUser = new Object() // singleton object  {}
 
 //const tinderUser = {} // non-singleton object {}
 
-console.log(tinderUser);
+//console.log(tinderUser);
 
 tinderUser.id = "123abc"
 tinderUser.name = "Anjali"
 tinderUser.isLoggedIn = false
 
-console.log(tinderUser); //{ id: '123abc', name: 'Anjali', isLoggedIn: false }
+//console.log(tinderUser); //{ id: '123abc', name: 'Anjali', isLoggedIn: false }
 
 const regularUser = {
     email:"amityadav@gmail.com",
@@ -20,17 +20,17 @@ const regularUser = {
         }
 }
 
-console.log(regularUser); // { email: 'amityadav@gmail.com' , fullname: { userfullname : {firstname : 'Akrati' , lastname: 'Kushwah'}}}
+// console.log(regularUser); // { email: 'amityadav@gmail.com' , fullname: { userfullname : {firstname : 'Akrati' , lastname: 'Kushwah'}}}
 
-console.log(regularUser.fullname); // { userfullname: { firstname: 'Akrati', lastname: 'Kushwah' } }
+// console.log(regularUser.fullname); // { userfullname: { firstname: 'Akrati', lastname: 'Kushwah' } }
 
-console.log(regularUser.fullname.userfullname.lastname); // Kushwah
+// console.log(regularUser.fullname.userfullname.lastname); // Kushwah
 
 const obj1 = {1 : "a" , 2: "b"}
 const obj2 = {3: "c" , 4 : "d"}
 
 const obj3 = Object.assign({} , obj1 , obj2) // targeted object is {} ,,, values of obj1 & obj2 is assigned to {}
-console.log(obj3); //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+//console.log(obj3); //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
 
 const obj4 = {...obj1 , ...obj2} //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
 
@@ -53,15 +53,37 @@ const users = [
     }
 ]
 
-console.log(users[1].email) //Akrati.kushwah@gmail.com
-console.log(tinderUser); //{ id: '123abc', name: 'Anjali', isLoggedIn: false }
+// console.log(users[1].email) //Akrati.kushwah@gmail.com
+// console.log(tinderUser); //{ id: '123abc', name: 'Anjali', isLoggedIn: false }
 
-console.log(Object.keys(tinderUser)); //[ 'id', 'name', 'isLoggedIn' ]
-console.log(Object.values(tinderUser)); //[ '123abc', 'Anjali', false ]
-console.log(Object.entries(tinderUser)); //[ [ 'id', '123abc' ], [ 'name', 'Anjali' ], [ 'isLoggedIn', false ] ]
+// console.log(Object.keys(tinderUser)); //[ 'id', 'name', 'isLoggedIn' ]
+// console.log(Object.values(tinderUser)); //[ '123abc', 'Anjali', false ]
+// console.log(Object.entries(tinderUser)); //[ [ 'id', '123abc' ], [ 'name', 'Anjali' ], [ 'isLoggedIn', false ] ]
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn')); //true
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); //true
 
+
+const course = {
+    courseName : "JS in hindi" , 
+    courseInstructor : "Hitesh" , 
+    coursePrice : "999"
+}
+
+
+//course.courseInstructor
+
+const {courseInstructor : instructor} = course
+
+//console.log(courseInstructor); // Hitesh
+
+console.log(instructor); //     Hitesh 
+
+
+const navbar = ({company}) => {
+
+}
+
+navbar(company = "Hitesh")
 
 
 
