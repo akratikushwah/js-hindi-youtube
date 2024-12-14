b504b8198310e2c9b743346a97ba8b19bb60d1b5
Akrati Kushwah <70066917+akratikushwah@users.noreply.github.com>
2024-12-14 01:31:54 +0000

    for of

---
+++ 05_iterations/three.js
@@ -0,0 +1,18 @@
+// for of
+
+// ["" , "" , ""]
+// [{} , {} , {}]
+
+const arr = [1 ,2 , 3, 4, 5]
+
+for (const num of arr) {
+    console.log(num); // 1 2 3 4 5
+    
+}
+
+const greetings = "Hello World!"
+
+for (const greet of greetings) {
+    console.log(greet); // Hello World!
+    
+}
